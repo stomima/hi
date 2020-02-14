c962a1512b1d700cfc387cc60eda875c19a0d539
saint <saint@StomimaMAC.local>
2020-02-14 11:58:16 +0800

    Add new file

---
+++ hi.py
@@ -0,0 +1,11 @@
+price = 10
+pen = 0.35
+rain = True
+name = 'Saint'
+
+print(price,pen,rain,name)
+name = input ('Please input your NAME:')
+weight = input ('Please input your WEIGHT:')
+height = input ('Please input your HEIGHT:')
+print ('Good morning,',name,'你的體重',weight,'你的身高',height)
+
